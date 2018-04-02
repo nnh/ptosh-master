84b33426c71106c50c73e090ae9c00f64a4acb72
kirimichan <mariko.ohtsuka@nnh.go.jp>
2018-04-02 16:13:05 +0900

    narrow down llt_name

--- programs/ptosh-master.R
+++ programs/ptosh-master.R
@@ -25,18 +25,25 @@ ResDuplicated <- function(df, sortkey, output_columns){
   }
   return(subset(temp_df, temp_df$check_f == T, output_columns))
 }
+SortDF <- function(df, sortkey1, sortkey2){
+  # データフレームdfをsortkey1昇順,sortkey2降順でソートして返す
+  sortlist <- order(df[ ,sortkey1], df[ ,sortkey2], decreasing=T)
+  df_sort <- df[sortlist, ]
+  return(df_sort)
+}
 WriteCsv <- function(df, output_path, output_csvname){
-  write.csv(df, paste(output_path, output_csvname, sep="/"), na='""', row.names=F)
+  write.csv(df, paste(output_path, output_csvname, sep="/"), na='""', row.names=F, fileEncoding="CP932")
   return(NA)
 }
 #######################
 # Constant definition #
 #######################
+Sys.setenv("TZ" = "Asia/Tokyo")
 # MedDRAバージョン指定
 kVersion <- "V21.0"
 kOutput_foldername <- "output_ptosh_option"
-input_prt_path <- "//aronas/References/NMC ISR 情報システム研究室/MedDRA"
-output_prt_path <- "//aronas/Projects/NMC ISR 情報システム研究室/MedDRA"
+input_prt_path <- "/Volumes/Projects/NMC ISR 情報システム研究室/MedDRA"
+output_prt_path <- "/Volumes/Projects/NMC ISR 情報システム研究室/MedDRA"
 # input csv name
 kLlt_j_csv <- "llt_j.asc"
 kLlt_csv <- "llt.asc"
@@ -65,7 +72,7 @@ kCtcae_colname <- c("seq", "llt_code", "soc", "soc_kanji", "llt_name", "llt_kanj
 ########################################################################################################################
 # パス設定
 # MedDRA\Vxx.x\ASCII\MDRA_Jxxx(xxxはバージョン毎に異なる)に入力ファイルが格納されている
-input_path <- paste0(input_prt_path, "/", kVersion, "/", "ASCII")
+input_path <- paste0(input_prt_path, "/", kVersion, "/", "ASCII/")
 temp_input_list <- list.files(input_path)
 if (length(temp_input_list) == 1) {
   input_path <- paste0(input_path, "/", temp_input_list)
@@ -73,9 +80,9 @@ if (length(temp_input_list) == 1) {
   input_path <- NA
 }
 output_path <- paste0(output_prt_path, "/", kVersion, "/", kOutput_foldername)
-ctcae_path <- "//aronas/Projects/NMC ISR 情報システム研究室/MedDRA/V21.0/CTCAE"  # CTCAEデータ
+ctcae_path <- "/Volumes/Projects/NMC ISR 情報システム研究室/MedDRA/V21.0/CTCAE"  # CTCAEデータ
 # 出力フォルダが存在しなければ作成
-if (!(file.exists(output_path))) {
+if (file.exists(output_path) == F) {
   dir.create(output_path)
 }
 # MedDRA、CTCAEフォルダ配下のファイル存在チェック
@@ -85,13 +92,16 @@ if (!is.na(input_path)) {
     ctcae_file_list <- list.files(ctcae_path, full.names=T)
     # CTCAEファイルの読み込みまで成功すれば処理続行
     if (length(ctcae_file_list) == 1) {
-      ctcae_df <- read_excel(ctcae_file_list[1], sheet=1, col_names=T)
-      if (exists("ctcae_df")) {
+      df_ctcae <- read_excel(ctcae_file_list[1], sheet=1, col_names=T)
+      if (exists("df_ctcae")) {
         file_existence_f <- T
         # 列名に改行が入っているため再設定
-        names(ctcae_df) <- kCtcae_colname
+        names(df_ctcae) <- kCtcae_colname
         # CTCAE 見出し行等を除去
-        ctcae_df <- subset(ctcae_df, !is.na(ctcae_df$soc))
+        df_ctcae <- subset(df_ctcae, !is.na(df_ctcae$soc))
+        # コード順にソート
+        sortlist <- order(df_ctcae$llt_code)
+        df_ctcae <- df_ctcae[sortlist, ]
       }
     }
   }
@@ -102,7 +112,7 @@ if (file_existence_f == T){
   ##########
   # MDHIER読み込み
   mdhier_path <- paste0(input_path, "/", kMdhier_csv)
-  temp_mdhier <- read.csv(mdhier_path, as.is=T, sep="$", header=F)
+  temp_mdhier <- read.csv(mdhier_path, as.is=T, sep="$", header=F, fileEncoding="CP932")
   names(temp_mdhier) <- kMdhier_colname
   # primary_soc_fg="Y"のレコードのみ対象とする
   df_mdhier <- subset(temp_mdhier, primary_soc_fg == "Y")
@@ -113,18 +123,21 @@ if (file_existence_f == T){
   #######
   # LLT_J読み込み
   llt_j_path <- paste0(input_path, "/", kLlt_j_csv)
-  df_llt_j <- read.csv(llt_j_path, as.is=T, sep="$", header=F)
+  df_llt_j <- read.csv(llt_j_path, as.is=T, sep="$", header=F, fileEncoding="CP932")
   names(df_llt_j) <- kLlt_j_colname
-  # llt_code, llt_kanjiのみ出力
-  df_llt_j <- df_llt_j[c("llt_code", "llt_kanji")]
+  # llt_code, llt_kanji, llt_jcurrのみ出力
+  df_llt_j <- df_llt_j[c("llt_code", "llt_kanji", "llt_jcurr")]
   # LLT読み込み
   llt_path <- paste0(input_path, "/", kLlt_csv)
-  temp_llt <- read.csv(llt_path, as.is=T, sep="$", header=F)
+  temp_llt <- read.csv(llt_path, as.is=T, sep="$", header=F, fileEncoding="CP932")
   names(temp_llt) <- kLlt_colname
-  # llt_code, pt_code, llt_name, llt_currencyのみ出力
-  temp_llt <- temp_llt[c("llt_code", "pt_code", "llt_name", "llt_currency")]
+  # CTCAEとマージ CTCAEに存在しないレコードはseqに-1をセット
+  df_llt_ctcae_merge <- merge(temp_llt, df_ctcae[ ,c("llt_code", "seq")], by="llt_code", all.x=T)
+  df_llt_ctcae_merge$seq <- ifelse(is.na(df_llt_ctcae_merge$seq), -1, df_llt_ctcae_merge$seq)
+  # llt_code, pt_code, llt_name, llt_currency, seqのみ出力
+  df_llt_ctcae_merge <- df_llt_ctcae_merge[c("llt_code", "pt_code", "llt_name", "llt_currency", "seq")]
   # llt_currency="Y"のレコードのみ対象とする
-  df_llt <- subset(temp_llt, llt_currency == "Y")
+  df_llt <- subset(df_llt_ctcae_merge, llt_currency == "Y")
   # LLTとLLT_Jをllt_codeでマージ
   df_llt_merge <- merge(df_llt, df_llt_j, by="llt_code", all.x=T)
   # hierarchyとpt_codeでマージ
@@ -136,13 +149,13 @@ if (file_existence_f == T){
   ######
   # PT_J読み込み
   pt_j_path <- paste0(input_path, "/", kpt_j_csv)
-  df_pt_j <- read.csv(pt_j_path, as.is=T, sep="$", header=F)
+  df_pt_j <- read.csv(pt_j_path, as.is=T, sep="$", header=F, fileEncoding="CP932")
   names(df_pt_j) <- kPt_j_colname
   # pt_code, pt_kanjiのみ出力
   df_pt_j <- df_pt_j[c("pt_code", "pt_kanji")]
   # PT読み込み
   pt_path <- paste0(input_path, "/" ,kpt_csv)
-  df_pt <- read.csv(pt_path, as.is=T, sep="$", header=F)
+  df_pt <- read.csv(pt_path, as.is=T, sep="$", header=F, fileEncoding="CP932")
   names(df_pt) <- kPt_colname
   # pt_code, pt_soc_code, pt_nameのみ出力
   df_pt <- df_pt[c("pt_code", "pt_soc_code", "pt_name")]
@@ -157,13 +170,13 @@ if (file_existence_f == T){
   #######
   # soc_J読み込み
   soc_j_path <- paste0(input_path, "/", kSoc_j_csv)
-  df_soc_j <- read.csv(soc_j_path, as.is=T, sep="$", header=F)
+  df_soc_j <- read.csv(soc_j_path, as.is=T, sep="$", header=F, fileEncoding="CP932")
   names(df_soc_j) <- kSoc_j_colname
   # soc_code, soc_kanjiのみ出力
   df_soc_j <- df_soc_j[c("soc_code", "soc_kanji")]
   # soc読み込み
   soc_path <- paste0(input_path, "/", kSoc_csv)
-  df_soc <- read.csv(soc_path, as.is=T, sep="$", header=F)
+  df_soc <- read.csv(soc_path, as.is=T, sep="$", header=F, fileEncoding="CP932")
   names(df_soc) <- kSoc_colname
   # soc_code, soc_nameのみ出力
   df_soc <- df_soc[c("soc_code", "soc_name")]
@@ -177,9 +190,44 @@ if (file_existence_f == T){
   df_all_merge <- merge(df_llt_pt_merge, df_soc_merge, by.x="pt_soc_code", by.y="soc_code", all.x=T)
   # 列名pt_soc_code->soc_codeに変更する
   df_all_merge <- ReplaceNames(df_all_merge, "pt_soc_code", "soc_code")
+  # 日本語重複病名の削除
+  # うつ病->Depression, Depression mental, Depressive illnessのように日本語と英語が1:nになっている病名への対応
+  # CTCAEに存在する病名は、そのLLT_CODEのみ出力対象とする
+  # 上記に当てはまらないものについては日本語カレンシーフラグがYのレコードを出力する
+  # llt_kanji昇順, seq降順でソート
+  df_sort_all_merge <- SortDF(df_all_merge, "llt_kanji", "seq")
+  save_llt_kanji <- ""
+  df_sort_all_merge$delete_f <- F
+  for (i in 1:length(df_sort_all_merge$llt_code)) {
+    # 同一日本語病名でseq>0のレコードがある場合、seq=-1のレコードは削除する
+    if (df_sort_all_merge[i, "seq"] > 0) {
+      # llt_kanjiを退避
+      save_llt_kanji <- df_sort_all_merge[i, "llt_kanji"]
+    } else {
+      if (save_llt_kanji == df_sort_all_merge[i, "llt_kanji"]){
+        df_sort_all_merge[i, "delete_f"] <- T
+      }
+    }
+  }
+  df_all_merge_ctcae <- subset(df_sort_all_merge, delete_f == F)
+  # llt_kanji昇順, llt_jcurr降順でソート
+  save_llt_kanji <- ""
+  df_sort_all_merge_ctcae <- SortDF(df_all_merge_ctcae, "llt_kanji", "llt_jcurr")
+  for (i in 1:length(df_sort_all_merge_ctcae$llt_code)) {
+    # 同一日本語病名の場合日本語カレンシーフラグがNのレコードを削除する
+    if (df_sort_all_merge_ctcae[i, "llt_jcurr"] == "Y") {
+      save_llt_kanji <- df_sort_all_merge_ctcae[i, "llt_kanji"]
+    } else {
+      if (save_llt_kanji == df_sort_all_merge_ctcae[i, "llt_kanji"]) {
+        df_sort_all_merge_ctcae[i, "delete_f"] <- T
+      }
+    }
+  }
+  df_output_all_merge <- subset(df_sort_all_merge_ctcae, delete_f == F)
   # 出力列順の変更
-  df_output <- df_all_merge[c("soc_code", "pt_code", "llt_code", "soc_name", "soc_kanji", "pt_name",
-                              "pt_kanji", "llt_name", "llt_kanji", "llt_currency", "pt_primary_soc_fg", "llt_primary_soc_fg")]
+  df_output <- df_output_all_merge[c("soc_code", "pt_code", "llt_code", "soc_name", "soc_kanji", "pt_name",
+                              "pt_kanji", "llt_name", "llt_kanji", "llt_currency", "pt_primary_soc_fg",
+                              "llt_primary_soc_fg", "llt_jcurr")]
   ####################
   # edit output data #
   ####################
